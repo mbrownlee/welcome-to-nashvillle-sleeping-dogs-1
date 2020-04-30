8b5359126085cfda6f2cd676f0f4f524d2e28a49
Adrian Garmendia <dejima@gmail.com>
2020-04-30 11:13:37 -0500

    craziness

--- src/scripts/parks/parkITIN.js
+++ src/scripts/parks/parkITIN.js
@@ -1,17 +1,36 @@
-/* //dependencies: parksAPI.js
+//dependencies: parksAPI.js
 
+const saveResults = document
+	.querySelector(".search-results__container")
+	.addEventListener("click", (saveEvent) => {
+		if (saveEvent.target.id.includes("btn-parks")) {
+			console.log(
+				document.querySelector(`#park__${saveEvent.target.id.split("__")[1]}`)
+					.innerHTML
+			);
+			// if (document.querySelector(".dropdown").value === "" || document.querySelector(".attractionDropdown").value === "" || document.querySelector(".eatDropdown").value === "") {
+			//   alert("Please complete your Itinerary")
+			// } else {
+			//   const selectedPark = document.querySelector(".dropdown").value
+			//   const [parkCode, parkState] = document.querySelector(".dropdown").value.split("--")
+			//   const foundPark = parkCollection.find(
+			//     (singlePark) => {
+			//       return singlePark.parkCode === parkCode
+			//     }
+			//   )
+			//   const parkName = foundPark.fullName
+			//   const savedTrip = {
+			//     park: parkName,
+			//     bizzare: document.querySelector(".attractionDropdown").value,
+			//     eatery: document.querySelector(".eatDropdown").value
+			//   }
+			//           saveTrip(savedTrip).then(
+			//             () => {
+			//               ItineraryListComponent()
+			//             }
+			//             )
+			//           }
 
-
-document.querySelector("#save-btn__parks").addEventListener("click", () => {
-            const savedPark = document.querySelector(".park-itinerary__container")
-
-            return `<h3>${parksItinerary}
-            </h3>
-`
-            
-savedPark.innerHTML
-
-
-}
-
- */
+			//
+		}
+	});
