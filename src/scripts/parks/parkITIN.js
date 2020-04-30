7a7ae8da216415fc70c479e598ebf36628b0f28c
Kirk Sudduth <kirksudduth@gmail.com>
2020-04-30 11:20:10 -0500

    tried to fix some stuff

--- src/scripts/parks/parkITIN.js
+++ src/scripts/parks/parkITIN.js
@@ -2,16 +2,25 @@
 
 
 
-document.querySelector("#save-btn__parks").addEventListener("click", () => {
-            const savedPark = document.querySelector(".park-itinerary__container")
-
+// document.querySelector("#save-btn__parks").addEventListener("click", () => {
+            
+    const savedPark = document.querySelector(".park-itinerary__container")
             return `<h3>${parksItinerary}
             </h3>
-`
-            
-savedPark.innerHTML
+`savedPark.innerHTML
 
 
 }
 
  */
+const makeItinFunction = () => {
+  let saveItin = event.target;
+  document.querySelector(
+    ".park-itinerary__container"
+  ).innerHTML = `<p>${saveItin}.h2</p>`;
+};
+let saveParkButton = document.getElementById("#save-btn__parks");
+
+saveParkButton.addEventListener("click", () => {
+  makeItinFunction();
+});
--- src/scripts/parks/parksAPI.js
+++ src/scripts/parks/parksAPI.js
@@ -2,28 +2,28 @@
 const parksAPI = document.querySelector(".search-results__container");
 //fetch gets the data from the database
 document.querySelector("#search-btn__parks").addEventListener("click", () => {
-	const parksURL = "https://data.nashville.gov/resource/74d7-b74t.json?$q=";
-	const parkUserSearch = document.querySelector("#search-input__parks").value;
-	document.querySelector(".search-results__container").innerHTML = "";
-	fetch(`${parksURL}` + `${parkUserSearch}`)
-		.then((parks) => parks.json())
-		.then((parsedParks) => {
-			//THIS IS THE ONLY PLACE WHERE I HAVE ACCESS TO THE DATA FROM THE API
-			console.log(parsedParks);
-			//RETURNS AN ARRAY. THEN NEED TO ITERATE THE ARRAY.
-			for (const park of parsedParks) {
-				// parse the address into a to be dot notted into
-				park.newAddress = JSON.parse(park.mapped_location.human_address);
-				//I NEED TO CONVERT EVERY OBJECT TO A STRING. FUNCTION MAKEPARKLIST DOES THAT FOR ME.
-				const parkList = makeParkList(park);
-				//NOW I HAVE SRING REPRESENTATION I NEED TO STORE IT IN THE QUERY ABOVE
-				parksAPI.innerHTML += parkList;
-			}
-		});
+  const parksURL = "https://data.nashville.gov/resource/74d7-b74t.json?$q=";
+  const parkUserSearch = document.querySelector("#search-input__parks").value;
+  document.querySelector(".search-results__container").innerHTML = "";
+  fetch(`${parksURL}` + `${parkUserSearch}`)
+    .then((parks) => parks.json())
+    .then((parsedParks) => {
+      //THIS IS THE ONLY PLACE WHERE I HAVE ACCESS TO THE DATA FROM THE API
+      console.log(parsedParks);
+      //RETURNS AN ARRAY. THEN NEED TO ITERATE THE ARRAY.
+      for (const park of parsedParks) {
+        // parse the address into a to be dot notted into
+        park.newAddress = JSON.parse(park.mapped_location.human_address);
+        //I NEED TO CONVERT EVERY OBJECT TO A STRING. FUNCTION MAKEPARKLIST DOES THAT FOR ME.
+        const parkList = makeParkList(park);
+        //NOW I HAVE SRING REPRESENTATION I NEED TO STORE IT IN THE QUERY ABOVE
+        parksAPI.innerHTML += parkList;
+      }
+    });
 });
 //CONVERTS OBJECT FROM INSIDE THE ARRAY RETURNED FROM FETCH INTO A STRING
 function makeParkList(park) {
-	return `
+  return `
 				<h2>${park.park_name}</h2>
 				<p>Doge friendly: ${park.dog_park}</p>
 				<p>${park.newAddress.address} ${park.newAddress.city} ${park.newAddress.state}</p>
