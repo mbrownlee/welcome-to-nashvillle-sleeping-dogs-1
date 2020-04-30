63807b5048b97a76264fbc03593a8bf566c3b284
Adrian Garmendia <dejima@gmail.com>
2020-04-30 11:15:14 -0500

    craziness

--- src/scripts/parks/parkITIN.js
+++ src/scripts/parks/parkITIN.js
@@ -8,29 +8,5 @@ const saveResults = document
 				document.querySelector(`#park__${saveEvent.target.id.split("__")[1]}`)
 					.innerHTML
 			);
-			// if (document.querySelector(".dropdown").value === "" || document.querySelector(".attractionDropdown").value === "" || document.querySelector(".eatDropdown").value === "") {
-			//   alert("Please complete your Itinerary")
-			// } else {
-			//   const selectedPark = document.querySelector(".dropdown").value
-			//   const [parkCode, parkState] = document.querySelector(".dropdown").value.split("--")
-			//   const foundPark = parkCollection.find(
-			//     (singlePark) => {
-			//       return singlePark.parkCode === parkCode
-			//     }
-			//   )
-			//   const parkName = foundPark.fullName
-			//   const savedTrip = {
-			//     park: parkName,
-			//     bizzare: document.querySelector(".attractionDropdown").value,
-			//     eatery: document.querySelector(".eatDropdown").value
-			//   }
-			//           saveTrip(savedTrip).then(
-			//             () => {
-			//               ItineraryListComponent()
-			//             }
-			//             )
-			//           }
-
-			//
 		}
 	});
