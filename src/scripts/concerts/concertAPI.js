4f572964b4adb139652fb5c40bdb76b75033153b
mbrownlee <michelle@orbwave.com>
2020-04-30 16:00:47 -0500

    concertAPI

--- src/scripts/concerts/concertAPI.js
+++ src/scripts/concerts/concertAPI.js
@@ -1,35 +1,37 @@
 const concertsAPI = document.querySelector(".search-results__container");
-//fetch gets the data from the database
+
 document.querySelector("#search-btn__concerts").addEventListener("click", () => {
-  const parksURL = "https://data.nashville.gov/resource/74d7-b74t.json?$q=";
-  const concertUserSearch = document.querySelector("#search-input__concerts").value;
+
+let apiUrl = app_keys.app_url
+let apiKey = app_keys.app_key
+
+const concertUserSearch = document.querySelector("#search-input__concerts").value;
   document.querySelector(".search-results__container").innerHTML = "";
-  fetch(`${concertsURL}` + `${concertUserSearch}`)
+  fetch(`${apiUrl}${apiKey}&keyword=${concertUserSearch}&local=*&city=Nashville&stateCode=TN`)
     .then((concerts) => concerts.json())
     .then((parsedConcerts) => {
       //THIS IS THE ONLY PLACE WHERE I HAVE ACCESS TO THE DATA FROM THE API
       console.log(parsedConcerts);
       //RETURNS AN ARRAY. THEN NEED TO ITERATE THE ARRAY.
-      for (let i = 0; i < parsedConcerts.length; i++) {
-        // parse the address into a to be dot notted into
-        /*parsedParks[i].newAddress = JSON.parse(
-          parsedParks[i].mapped_location.human_address
-        ); */
-        //I NEED TO CONVERT EVERY OBJECT TO A STRING. FUNCTION MAKEPARKLIST DOES THAT FOR ME.
-        parsedConcerts[i].id = i;
-        const ConcertList = makeConcertListComponent(parsedConcerts[i]);
-        //NOW I HAVE SRING REPRESENTATION I NEED TO STORE IT IN THE QUERY ABOVE
-        concertsAPI.innerHTML += concertList;
+      const events = parsedConcerts._embedded.events
+      for (let i = 0; i < events.length; i++) {
+        
+        
+        //console.log(events[i])
+        const concertList = makeConcertListComponent(events[i]);
+      
+       concertsAPI.innerHTML += concertList;
       }
+      
     });
 });
 //CONVERTS OBJECT FROM INSIDE THE ARRAY RETURNED FROM FETCH INTO A STRING
-function makeConcertListComponent(park) {
+function makeConcertListComponent(concert) {
+    console.log("howdy", concert)
   return `
 			<section>
-				<div id="park__${park.id}">${park.park_name}</div>
-				<div>Doge friendly: ${park.dog_park}</div>
-				<div>${park.newAddress.address} ${park.newAddress.city} ${park.newAddress.state}</div>
-				<button id="btn-parks__${park.id}">Save</button>
-			</section>`;
+				<div id="concert__${concert.id}">${concert.name}</div>
+				<div>Venue: ${concert._embedded.venues[0].name}</div>
+				<button id="btn-concerts__${concert.id}">Save</button>
+			</section>`; 
 }
---
+++ src/scripts/concerts/keys.js
@@ -0,0 +1,6 @@
+const app_keys = {
+    app_url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=",
+    app_key: "OBvCvwNLB6lABAdVGhNoNmlLQCmd0iIm"
+}
+
+ 
