c5b541eb2c72082aa0eb8d6717940f0a563425b3
Kirk Sudduth <kirksudduth@gmail.com>
2020-04-30 13:21:37 -0500

    parks itinerary

--- src/scripts/parks/parkITIN.js
+++ src/scripts/parks/parkITIN.js
@@ -4,9 +4,12 @@ const saveResults = document
   .querySelector(".search-results__container")
   .addEventListener("click", (saveEvent) => {
     if (saveEvent.target.id.includes("btn-parks")) {
-      console.log(
-        document.querySelector(`#park__${saveEvent.target.id.split("__")[1]}`)
-          .innerHTML
-      );
+      let parkSaveResult = document.querySelector(
+        `#park__${saveEvent.target.id.split("__")[1]}`
+      ).innerHTML;
+      let parkItinerary = document.querySelector(".park-itinerary__container");
+      parkItinerary.innerHTML = parkSaveResult;
     }
   });
+
+// document.querySelector(".park-itinerary__container").innerHTML = saveResults;
