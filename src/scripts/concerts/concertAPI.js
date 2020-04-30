44b54b4c71cd7a0a407d4b9245c9b06f28ba7289
mbrownlee <michelle@orbwave.com>
2020-04-30 14:38:24 -0500

    working more

--- src/scripts/concerts/concertAPI.js
+++ src/scripts/concerts/concertAPI.js
@@ -4,27 +4,27 @@ document.querySelector("#search-btn__concerts").addEventListener("click", () => 
   const parksURL = "https://data.nashville.gov/resource/74d7-b74t.json?$q=";
   const concertUserSearch = document.querySelector("#search-input__concerts").value;
   document.querySelector(".search-results__container").innerHTML = "";
-  fetch(`${parksURL}` + `${parkUserSearch}`)
-    .then((parks) => parks.json())
-    .then((parsedParks) => {
+  fetch(`${concertsURL}` + `${concertUserSearch}`)
+    .then((concerts) => concerts.json())
+    .then((parsedConcerts) => {
       //THIS IS THE ONLY PLACE WHERE I HAVE ACCESS TO THE DATA FROM THE API
-      console.log(parsedParks);
+      console.log(parsedConcerts);
       //RETURNS AN ARRAY. THEN NEED TO ITERATE THE ARRAY.
-      for (let i = 0; i < parsedParks.length; i++) {
+      for (let i = 0; i < parsedConcerts.length; i++) {
         // parse the address into a to be dot notted into
-        parsedParks[i].newAddress = JSON.parse(
+        /*parsedParks[i].newAddress = JSON.parse(
           parsedParks[i].mapped_location.human_address
-        );
+        ); */
         //I NEED TO CONVERT EVERY OBJECT TO A STRING. FUNCTION MAKEPARKLIST DOES THAT FOR ME.
-        parsedParks[i].id = i;
-        const parkList = makeParkListComponent(parsedParks[i]);
+        parsedConcerts[i].id = i;
+        const ConcertList = makeConcertListComponent(parsedConcerts[i]);
         //NOW I HAVE SRING REPRESENTATION I NEED TO STORE IT IN THE QUERY ABOVE
-        parksAPI.innerHTML += parkList;
+        concertsAPI.innerHTML += concertList;
       }
     });
 });
 //CONVERTS OBJECT FROM INSIDE THE ARRAY RETURNED FROM FETCH INTO A STRING
-function makeParkListComponent(park) {
+function makeConcertListComponent(park) {
   return `
 			<section>
 				<div id="park__${park.id}">${park.park_name}</div>
